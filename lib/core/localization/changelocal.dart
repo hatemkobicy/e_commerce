927c69a2ae901584a6b58f889380e7edd57828ab
Hatem Kobicy <hatem.kobicy@gmail.com>
2024-02-14 00:41:52 +0100

    modified:   android/app/src/main/AndroidManifest.xml 	new file:   lib/controller/address/add_controller.dart 	modified:   lib/core/class/statusrequest.dart 	modified:   lib/core/localization/changelocal.dart 	modified:   lib/view/screens/address/add_addres.dart

--- lib/core/localization/changelocal.dart
+++ lib/core/localization/changelocal.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce/core/constants/apptheme.dart';
 import 'package:e_commerce/core/services/services.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 
 class LocalController extends GetxController {
@@ -19,6 +20,7 @@ class LocalController extends GetxController {
 
   @override
   void onInit() {
+    requestpermissionLocation();
     String? sheardprefLang = appServices.sharedPreferences.getString("lang");
     if (sheardprefLang == "ar") {
       language = const Locale("ar");
@@ -31,5 +33,41 @@ class LocalController extends GetxController {
       apptheme = englishtheme;
     }
     super.onInit();
+  }
+
+  requestpermissionLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Get.snackbar("Location Permission",
+          "Location services are not enabled Please Enable it");
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Get.snackbar(
+            "Location permissions", "Location permissions are denied");
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Get.snackbar("Location permissions",
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
--- lib/view/screens/address/add_addres.dart
+++ lib/view/screens/address/add_addres.dart
@@ -1,11 +1,18 @@
+import 'package:e_commerce/controller/address/add_controller.dart';
+import 'package:e_commerce/core/class/statusrequest.dart';
 import 'package:e_commerce/core/constants/color.dart';
+import 'package:e_commerce/core/constants/imageassets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:lottie/lottie.dart';
 
 class AddAddres extends StatelessWidget {
   const AddAddres({super.key});
 
   @override
   Widget build(BuildContext context) {
+    AddAddressController controllerpage = Get.put(AddAddressController());
     return Scaffold(
       backgroundColor: AppColor.primery,
       appBar: AppBar(
@@ -14,7 +21,26 @@ class AddAddres extends StatelessWidget {
         centerTitle: true,
         title: const Text('New Addres'),
       ),
-      body: Container(),
+      body: Container(
+        child: GetBuilder<AddAddressController>(
+          builder: (controller) =>
+              controller.statusRequest == StatusRequest.loading
+                  ? Center(child: LottieBuilder.asset(AppImageAsset.loadding))
+                  : Column(
+            children: [
+             if(controllerpage.kGooglePlex != null) Expanded(
+                child: GoogleMap(
+                  mapType: MapType.normal,
+                  initialCameraPosition: controllerpage.kGooglePlex!,
+                  onMapCreated: (GoogleMapController controllermap) {
+                    controllerpage.completercontroller!.complete(controllermap);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
