c614e645f50999a97edccd3dcb7a92baafb030ec
Hatem Kobicy <hatem.kobicy@gmail.com>
2024-02-17 01:13:25 +0100

    modified:   lib/controller/address/finaladd_controller.dart 	modified:   lib/view/screens/address/final_addres.dart

--- lib/controller/address/finaladd_controller.dart
+++ lib/controller/address/finaladd_controller.dart
@@ -1,13 +1,19 @@
 import 'package:e_commerce/core/class/statusrequest.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class FinalAddAddressController extends GetxController {
   StatusRequest statusRequest = StatusRequest.loading;
-
+  TextEditingController? name;
+  TextEditingController? city;
+  TextEditingController? street;
   String? lat;
   String? long;
 
   initialData() {
+    name = TextEditingController();
+    city = TextEditingController();
+    street = TextEditingController();
     lat = Get.arguments['lat'];
     long = Get.arguments['long'];
     print(lat);
--- lib/view/screens/address/final_addres.dart
+++ lib/view/screens/address/final_addres.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce/controller/address/finaladd_controller.dart';
 import 'package:e_commerce/core/constants/color.dart';
+import 'package:e_commerce/view/widgets/Buttons/mbuttons.dart';
+import 'package:e_commerce/view/widgets/textform/custometextform.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -18,7 +20,130 @@ class FinalAddres extends StatelessWidget {
         centerTitle: true,
         title: const Text('Complate Addres'),
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            child: TextFormField(
+              controller: controllerpage.street,
+              keyboardType: TextInputType.name,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal),
+              decoration: InputDecoration(
+                alignLabelWithHint: true,
+                hintStyle: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal),
+                hintText: "Street name",
+                prefixIcon: const Icon(
+                  Icons.streetview_rounded,
+                  color: AppColor.onboardingcolor,
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(130, 117, 173, 242),
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            child: TextFormField(
+              controller: controllerpage.city,
+              keyboardType: TextInputType.name,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal),
+              decoration: InputDecoration(
+                alignLabelWithHint: true,
+                hintStyle: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal),
+                hintText: "city name",
+                prefixIcon: const Icon(
+                  Icons.location_city_rounded,
+                  color: AppColor.onboardingcolor,
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(130, 117, 173, 242),
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            child: TextFormField(
+              controller: controllerpage.name,
+              keyboardType: TextInputType.name,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal),
+              decoration: InputDecoration(
+                alignLabelWithHint: true,
+                hintStyle: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal),
+                hintText: "Addres name",
+                prefixIcon: const Icon(
+                  Icons.person_pin_circle_rounded,
+                  color: AppColor.onboardingcolor,
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(130, 117, 173, 242),
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 400,
+          ),
+          Container(
+            width: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadiusDirectional.circular(40),
+              color: AppColor.backgroundcolor1,
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            height: 60,
+            child: MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusDirectional.circular(40)),
+              onPressed: () {},
+              child: Text(
+                "Save Addres",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
